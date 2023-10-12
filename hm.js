9abb31f2d5efcebe26b4d469cd58b0889026a842
kavya <kavyasrir405@gmail.com>
2023-10-12 09:06:20 +0530

    hm

---
+++ hm.js
@@ -0,0 +1,11 @@
+
+
+
+function scrolr() {
+    let x=document.querySelector(".coll");
+    x.scrollBy(1000, 0);
+  }
+  function scrolll() {
+    let x=document.querySelector(".coll");
+    x.scrollBy(-1000, 0);
+  }
